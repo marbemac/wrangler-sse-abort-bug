fca1a0bae543d87c8b2b22379e4aa4af965d5c0a
Kacper Wojciechowski <39823706+jog1t@users.noreply.github.com>
2025-08-08 18:42:40 +0200

    refactor: export ActorHandler, use cf-workers lib without ambient env

--- worker/index.ts
+++ worker/index.ts
@@ -9,41 +9,44 @@ export type AgentNameParts = [OrgId, UserId];
 export type AgentName = Join<AgentNameParts, ":">;
 
 export const makeUserActor = async (nameParts: AgentNameParts) => {
-  return client.user.getOrCreate(nameParts.join(":"));
+	return client.user.getOrCreate(nameParts.join(":"));
 };
 
 export const user = actor({
-  state: { count: 0 },
-  actions: {
-    getCount: (c) => c.state.count,
-    increment: (c, amount: number = 1) => {
-      console.log("actorName", c.name);
-      c.state.count += amount;
-      c.broadcast("countChanged", c.state.count);
-      return c.state.count;
-    },
-  },
+	state: { count: 0 },
+	actions: {
+		getCount: (c) => c.state.count,
+		increment: (c, amount: number = 1) => {
+			console.log("actorName", c.name);
+			c.state.count += amount;
+			c.broadcast("countChanged", c.state.count);
+			return c.state.count;
+		},
+	},
 });
 
 const registry = setup({
-  use: { user },
+	use: { user },
 });
 
-const { client } = createServer(registry);
+const { client, createHandler } = createServer(registry);
+
+const { handler, ActorHandler } = createHandler();
+
+export { ActorHandler };
 
 export default {
-  async fetch(req) {
-    const url = new URL(req.url);
+	...handler,
+	fetch: async (request: Request, env, ctx) => {
+		const url = new URL(request.url);
 
-    if (url.pathname === "/debug/actor") {
-      const user = await makeUserActor(["org_1", "ag_1"]);
+		if (url.pathname === "/debug/actor") {
+			const user = await makeUserActor(["org_1", "ag_1"]);
 
-      await user.increment(1);
-      const newCount = await user.getCount();
-
-      return new Response(`new count ${newCount}`);
-    }
-
-    return new Response("Not found", { status: 404 });
-  },
-} satisfies ExportedHandler<Env>;
+			await user.increment(1);
+			const newCount = await user.getCount();
+			return new Response(`new count ${newCount}`);
+		}
+		return handler.fetch(request, env, ctx);
+	},
+} satisfies ExportedHandler;
